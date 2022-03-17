a77fb4b94b3c4e15cdc033ee3e4e1d980dbc4223
fakhuraibet <farah.kh120@gmail.com>
2022-03-17 18:58:33 +0300

    CW5 done
    done (done a bit late for personal reasons)

---
+++ script.js
@@ -0,0 +1,5 @@
+alert("Hi!My name is Farah Khuraibet");
+console.log("I am 14 years old");
+console.log(7 * 2);
+console.log("%cأنا في الصف التاسع في المرحلة الثانوية", "color:crimson");
+//JAVASCRIPT IS NOT JAVA(لخاطر نانسي)//

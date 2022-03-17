aaa6fa19048fb2ceb1860f04af63d9486529f671
fakhuraibet <farah.kh120@gmail.com>
2022-03-17 23:42:04 +0300

    Day 5 CW2
    cw2 of day 5 completed

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 alert("Hi!My name is Farah Khuraibet");
 console.log("I am 14 years old");
 console.log(7 * 2);
-console.log("%cأنا في الصف التاسع في المرحلة الثانوية", "color:crimson");
+console.log("%c أنا في الصف التاسع في المرحلة الثانوية", "color:crimson");
 //JAVASCRIPT IS NOT JAVA(لخاطر نانسي)//
+
+//second classwork//
+
+let budget = 20;
+console.log(budget > 30 && budget < 80);
+
+budget += 30;
+console.log(budget > 30 && budget < 80);

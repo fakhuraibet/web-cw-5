b6f48af809c190cad39d10bbf209bf1f9f29a003
fakhuraibet <farah.kh120@gmail.com>
2022-03-18 12:44:41 +0300

    CW3 Day 5

--- script.js
+++ script.js
@@ -2,12 +2,28 @@ alert("Hi!My name is Farah Khuraibet");
 console.log("I am 14 years old");
 console.log(7 * 2);
 console.log("%c أنا في الصف التاسع في المرحلة الثانوية", "color:crimson");
-//JAVASCRIPT IS NOT JAVA(لخاطر نانسي)//
+//JAVASCRIPT IS NOT JAVA(لخاطر نانسي)
 
-//second classwork//
+//second classwork
 
 let budget = 20;
 console.log(budget > 30 && budget < 80);
 
 budget += 30;
 console.log(budget > 30 && budget < 80);
+
+let grade = prompt("Enter your grade");
+
+if (grade >= 90) {
+  console.log("لقد حصلت على امتياز 🥳");
+} else if (grade >= 80) {
+  console.log("لقد حصلت على جيد جداً🤩");
+} else if (grade >= 70) {
+  console.log("لقد حصلت على جيد🙂");
+} else if (grade >= 60) {
+  console.log("لقد حصلت على مقبول😕");
+} else if (grade >= 50) {
+  console.log("لقد حصلت على ضعيف☹️");
+} else {
+  console.log("راسب💔");
+}
